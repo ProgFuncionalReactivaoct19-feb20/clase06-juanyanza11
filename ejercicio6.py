1161caf67139dafcdb547892704e904ea5ba27b5
juanyanza11 <ww4fjj@gmail.com>
2019-11-05 17:29:21 -0500

    Correccion linea 19, error en redundancia

--- ejercicio6.py
+++ ejercicio6.py
@@ -16,7 +16,7 @@ with open("data/trabajadores.csv") as midata:
 	funcion = filter(lambda x: len(list(x.items())[2][1])> 10, resultado)
 	print("Nombres con longitud mayor a 10\n",list(funcion),"\n\n")
 
-	newl = list(filter(lambda x: list(x.items())[1][1].split("-")[2], resultado)) # Lista en la posición de la fecha de nacimiento
+	newl = list(filter(lambda x: list(x.items()), resultado)) # Lista en la posición de la fecha de nacimiento
 	order = sorted(newl, key = lambda x: list(x.items())[1][1].split("-")[2]) # Ordenar por día de nacimiento
 
 	print("Ordenado por día de nacimiento\n",order)
